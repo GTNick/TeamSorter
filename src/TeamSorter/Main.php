d58261270505f8f120a92ff31d29e4359d65676d
TheDragonRing <tom.vines@rocketmail.com>
2016-05-06 20:20:14 +1000

    Fix?

--- src/TeamSorter/Main.php
+++ src/TeamSorter/Main.php
@@ -19,7 +19,7 @@ use pocketmine\item\Item;
 use pocketmine\Player;
 use pocketmine\Server;
 
-class TeamSorter extends PluginBase implements Listener{
+class Main extends PluginBase implements Listener{
     
     const NAME = "TeamSorter";
     const AUTHOR = "BoxOfDevs Team";
